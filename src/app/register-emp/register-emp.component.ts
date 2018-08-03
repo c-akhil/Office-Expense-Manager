2aef81f0b5ff01dbf82312245d0b8813f1f02802
AkhilSoft <akhilkumargoud@gmail.com3>
2018-08-03 18:48:15 +0530

    sal updated

--- src/app/register-emp/register-emp.component.ts
+++ src/app/register-emp/register-emp.component.ts
@@ -8,12 +8,13 @@ import { Http } from "@angular/http";
   styleUrls: ["./register-emp.component.css"]
 })
 export class RegisterEmpComponent implements OnInit {
-  url:string= "http://192.168.2.81:8080/OfficeExpenseManager/registration/registration";
+  url:string= "http://192.168.2.81:8080/OfficeExpenseManager/registration/registration1";
 
   form = new FormGroup({
     name: new FormControl("" ,Validators.required),
-
+    
     designation: new FormControl("" ,Validators.required),
+    salary:  new FormControl("" ,Validators.required),
     mobileNo: new FormControl("",[Validators.required]),
     email: new FormControl("",[Validators.required,Validators.email]),
     gender: new FormControl("",Validators.required),
@@ -44,12 +45,13 @@ export class RegisterEmpComponent implements OnInit {
       designation: this.form.value.designation,
       mobileNo: this.form.value.mobileNo,
       emailId: this.form.value.email,
+      salary:this.form.value.salary,
       gender: this.form.value.gender,
       password: this.form.value.password
     };
     console.log(JSON.stringify(emp));
 
-    this.http.post("http://192.168.2.81:8080/OfficeExpenseManager/registration/registration", emp).subscribe(response => {
+    this.http.post(this.url, emp).subscribe(response => {
       console.log(response.json());
 
       console.log(response);
